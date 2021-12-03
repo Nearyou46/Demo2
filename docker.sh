edfa8810a01761272c6422214045b328172b6f6e
root <root@localhost.localdomain>
2021-12-03 06:44:54 -0500

    fix

--- docker.sh
+++ docker.sh
@@ -12,6 +12,6 @@ sudo docker pull jenkins/jenkins
 sudo docker run --rm --name jenkins -d -p 8282:8080 -v /var/jenkins_home:/var/jenkins_home/ jenkins/jenkins
 sudo docker volume create db
 sudo docker pull us.gcr.io/gcp101233-lv61301devops/646/postgres_lv646:latest
-sudo docker run --name db -d -e POSTGRES_PASSWORD=password -p 5432:5432 -v db:/var/lib/postgresql/data/pgdata us.gcr.io/gcp101233-lv61301devops/646/postgres_lv646:latest
+sudo docker run --name db -d -e POSTGRES_PASSWORD -p 5432:5432 -v db:/var/lib/postgresql/data/pgdata us.gcr.io/gcp101233-lv61301devops/646/postgres_lv646:latest
 sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
